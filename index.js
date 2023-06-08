fb2fb8560a3f7112803441423d16f8817294caaf
Himanshu Singh <124182328+HiManShU-989@users.noreply.github.com>
2023-06-08 11:49:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var dice1 = ("images/dice" + randomNumber1 + ".png");
+var dice1 = ("dice" + randomNumber1 + ".png");
 document.querySelectorAll("img")[0].setAttribute("src", dice1);
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var dice2 = ("images/dice" + randomNumber2 + ".png");
+var dice2 = ("dice" + randomNumber2 + ".png");
 document.querySelectorAll("img")[1].setAttribute("src", dice2);
 
 if (randomNumber1 > randomNumber2) {
