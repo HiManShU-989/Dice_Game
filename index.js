a90c1ce5cb95aa84289ccb7e321e40fa31efd539
Himanshu Singh <124182328+HiManShU-989@users.noreply.github.com>
2024-05-28 13:19:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ if (randomNumber1 > randomNumber2) {
     document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!!!";
 }
 else if (randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩 Player 2 Wins!!!";
+    document.querySelector("h1").innerHTML = " Player 2 Wins 🚩!!!";
 }
 else {
     document.querySelector("h1").innerHTML = "Draw!!!";
